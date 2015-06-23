9628276af5f89adfd7aa833d6c4a5996124922b7
Mike Klein <michaelk@nete.com>
2015-06-23 22:24:39 +0000

    Dependencies in bower resolved on CI, ran build locally to ensure hinting and linting passed all tests passed, pushing to test CI.

--- src/client/app/drugs/drugs.controller.spec.js
+++ src/client/app/drugs/drugs.controller.spec.js
@@ -34,9 +34,9 @@ describe('DrugsController', function() {
                 expect(controller.drugs).to.have.length.above(0);
             });
 
-             it('should have drugs count of 10', function () {
-                 expect(controller.drugs).to.have.length(10);
-             });
+            it('should have drugs count of 10', function () {
+                expect(controller.drugs).to.have.length(10);
+            });
         });
     });
 });
