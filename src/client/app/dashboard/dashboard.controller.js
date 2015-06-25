2a707618d19056c4edd4a65be79fe4e47b8ef0ef
Mike Klein <michaelk@nete.com>
2015-06-25 23:13:26 +0000

    Some additions in the dashboard controller.

--- src/client/app/dashboard/dashboard.controller.js
+++ src/client/app/dashboard/dashboard.controller.js
@@ -10,6 +10,7 @@
     function DashboardController($q, dataservice, logger) {
         var vm = this;
         vm.drugs = [];
+        vm.drugs.charData = [];
         vm.title = 'Dashboard';
 
         activate();
@@ -25,6 +26,10 @@
         function getTop10Drugs() {
             return dataservice.getTop10Drugs().then(function (data) {
                 vm.drugs = data;
+                vm.drugs.chartData = {
+                    labels: ['ENBREL','REGULAR STRENGTH PAIN RELIEF','HUMIRA','ECOTRIN','BUFFERIN LOW DOSE BUFFERED ASPIRIN','BUFFERIN','BAYER ADVANCED ASPIRIN EXTRA STRENGTH','ENTERIC ASPIRIN','LOW DOSE ASPIRIN ENTERIC SAFETY COATED','ASCRIPTIN REGULAR STRENGTH BUFFERED ASPIRIN'],
+                    series: [219582,204860,169284,167516,167170,167170,149468,116464,83165,83033]    
+                }
                 return vm.drugs;
             });
         }
