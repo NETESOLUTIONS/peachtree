4894eed3bb64bbe38addde2d24db867676e889d3
DK <dkroot2@gmail.com>
2015-06-27 17:25:04 -0400

    Enhanced diagnostics and error reporting in DB functionality

--- src/server/routes.js
+++ src/server/routes.js
@@ -13,25 +13,40 @@ module.exports = router;
 //////////////
 
 function getData(req, res, next) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, res) {
-            done();
+    function reportError(err) {
+        console.error(err);
+        res.send('Error in database operation:<br/><pre>' + err + '</pre>');
+    }
+    
+    if (process.env.DATABASE_URL === undefined) {
+        reportError("Please define DATABASE_URL environment variable");
+    } else {
+        console.log("Connecting to " + process.env.DATABASE_URL + "...");
+        pg.connect(process.env.DATABASE_URL, function (err, client, done) {
             if (err) {
-                console.error(err);
-                res.send('Error ' + err);
+                reportError(err);    
             } else {
-                res.send(res.rows);
+                console.log("Connected.");
+                client.query('SELECT * FROM test_table', function (err, res) {
+                    done();
+                    if (err) {
+                        reportError(err);
+                    } else {
+                        res.send(res.rows);
+                    }
+                });
             }
         });
-    });
+    }
 }
+
 function getPeople(req, res, next) {
     res.status(200).send(data.people);
 }
 
 function getPerson(req, res, next) {
     var id = +req.params.id;
-    var person = data.people.filter(function(p) {
+    var person = data.people.filter(function (p) {
         return p.id === id;
     })[0];
 
